a78d4089da3ba62a96c43f9b0cedec2337819f12
Marcel-git666 <93435045+Marcel-git666@users.noreply.github.com>
2022-01-21 17:48:19 +0100

    Finished day 1

---
+++ AoC2015_Swift/day01.swift
@@ -0,0 +1,50 @@
+//
+//  day01.swift
+//  AoC2021
+//
+//  Created by Marcel Mravec on 17.12.2021.
+//
+
+
+import Foundation
+
+
+
+enum Day01 {
+    static func run() {
+        let input1 = readFile("Resources/day01.input")
+        print(input1)
+        print(part1(input1))
+        print(part2(input1))
+    }
+}
+
+
+func part1(_ input: String) -> Int {
+    let str = input.components(separatedBy: ")").count
+    let str2 = input.components(separatedBy: "(").count
+    
+    return str2-str
+    }
+
+func part2(_ input: String) -> Int {
+    var sum = 0
+    var position = 0
+    for (index, value) in input.enumerated() {
+        if value == "(" {
+            sum += 1
+        } else {
+            sum -= 1
+        }
+        if sum < 0 {
+            position = index + 1
+            break
+        }
+    }
+    
+    return position
+    
+}
+
+
+
--- AoC2015_Swift/main.swift
+++ AoC2015_Swift/main.swift
@@ -1,11 +1,19 @@
 //
 //  main.swift
-//  AoC2015_Swift
+//  AoC2021
 //
-//  Created by Marcel Mravec on 21.01.2022.
+//  Created by Marcel Mravec on 17.12.2021.
 //
 
-import Foundation
-
-print("Hello, World!")
-
+//print("Nyní běží den 1.")
+Day01.run()
+//print("Nyní běží den 2.")
+//Day02.run()
+//Day03.run()
+//Day04.run()
+//Day05.run()
+//Day06.run()
+//Day07.run()
+//Day08.run()
+//Day09.run()
+//Day10.run()
