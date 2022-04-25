92b8b95e531b9a61275c01608bb91c09f7141760
Marcel-git666 <93435045+Marcel-git666@users.noreply.github.com>
2022-04-25 21:13:58 +0200

    Day14 part 2 done.

--- AoC2015_Swift/day14.swift
+++ AoC2015_Swift/day14.swift
@@ -13,15 +13,20 @@ enum Day14 {
         print(input)
         let lines = input.lines
         var reindeers: [Reindeer] = []
+        var reindeerStars: [ReindeerStar] = []
         var reindeer: Reindeer
+        var reindeerStar: ReindeerStar
         for line in lines {
             let components = line.split(separator: " ")
             reindeer = Reindeer.init(name: String(components[0]), speed: Int(components[3])!, lengthOfFly: Int(components[6])!, rest: Int(components[13])!)
+            reindeerStar = ReindeerStar.init(name: String(components[0]), speed: Int(components[3])!, lengthOfFly: Int(components[6])!, rest: Int(components[13])!, distanceTravelled: 0)
             reindeers.append(reindeer)
         }
         print(reindeers)
         let l = letThemFly(r: reindeers)
         print("Part 1 vzdálenost je \(l).")
+        let r = letThemFlyTwo(r: reindeers)
+        print("Part 2 počet bodíků je \(r).")
     }
 }
 
@@ -37,6 +42,27 @@ struct Reindeer {
         self.rest = rest
     }
 }
+
+struct ReindeerStar {
+    var name: String
+    var speed: Int
+    var lengthOfFly: Int
+    var rest: Int
+    var distanceTravelled: Int
+    init(name: String, speed: Int, lengthOfFly: Int, rest: Int, distanceTravelled: Int) {
+        self.name = name
+        self.speed = speed
+        self.lengthOfFly = lengthOfFly
+        self.rest = rest
+        self.distanceTravelled = distanceTravelled
+    }
+    mutating func changeDistance(distanceTravelled: Int) {
+        
+        self.distanceTravelled = distanceTravelled
+    }
+}
+
+
 
 func letThemFly(r: [Reindeer]) -> Int {
     let endTime = 2503
@@ -68,4 +94,36 @@ func letThemFly(r: [Reindeer]) -> Int {
     return totalDistance.max()!
 }
     
+func letThemFlyTwo(r: [Reindeer]) -> Int {
+    let endTime = 2503
+    var totalDistance : [Int] = []
+    var distance = 0
+    var points : [String : Int] = ["Comet":0, "Dancer":0, "Rudolph":0, "Cupid":0, "Prancer":0, "Donner":0, "Dasher":0, "Blitzen":0, "Vixen":0]
+    var distanceTravelled: [String : Int] = ["Comet":0, "Dancer":0, "Rudolph":0, "Cupid":0, "Prancer":0, "Donner":0, "Dasher":0, "Blitzen":0, "Vixen":0]
+    var isFlying = true
+    for i in 0..<endTime {
+        for deer in r {
+            if (i % (deer.lengthOfFly + deer.rest)) < deer.lengthOfFly {
+                isFlying = true
+            } else {
+                isFlying = false
+            }
 
+            if isFlying {
+                
+                distanceTravelled[deer.name]! += deer.speed
+            }
+        }
+//        print("Distance travelled je \(distanceTravelled).")
+        let maxOfRound = distanceTravelled.max { $0.value < $1.value }
+        let winnerOfRound = distanceTravelled.filter({ $0.value >= maxOfRound!.value })
+        for (key, value) in winnerOfRound {
+//            print("Winner is \(key).")
+            points[key]! += 1
+//            print("Bodíky za kolo \(i): \(points).")
+        }
+        
+    }
+    let winner = points.max { a, b in a.value < b.value }
+    return winner?.value ?? 11
+}
